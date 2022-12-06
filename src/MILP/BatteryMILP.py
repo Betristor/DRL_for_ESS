d1fe398d21131480c929fb22388fef4c8117c4d5
Yuheng Zhang <2845024327@qq.com>
2022-12-06 23:27:42 +0800

    modularize battery milp

--- src/MILP/BatteryMILP.py
+++ src/MILP/BatteryMILP.py
@@ -11,14 +11,11 @@ from pyomo.environ import (
     Reals,
     Objective,
     maximize,
-    SolverFactory
+    SolverFactory,
 )
-import matplotlib.pyplot as plt
 
-from battery.battery_efficiency import BatteryEfficiency
-from battery.battery_degradation import calculate_degradation
-import itertools
-
+from src.battery.battery_degradation import calculate_degradation
+from src.battery.battery_efficiency import BatteryEfficiency
 
 class BatteryMILP:
     def __init__(self, battery_power, battery_capacity):
@@ -224,75 +221,3 @@ class BatteryMILP:
     def update_remaining_cap(self, cycle_num):
         remaining_cap = calculate_degradation(cycle_num)
         return remaining_cap
-
-
-price_data = pd.read_csv("/Users/yuhengzhang/Documents/博一上/Foundations of RL/Project/DRL_for_ESS/data/PGF1_2_PDRP88-APND_prices.csv")
-
-# declare battery config
-battery_power = 10 # MW
-battery_capacity = 20 # MWh
-
-# declare intial soc
-soc = 0.5 * battery_capacity
-current_cycle = 0
-remaining_capacity = 100
-previous_ep_power = 0
-
-# Instaniate MILP battery object with price data
-a = BatteryMILP(battery_power, battery_capacity)
-
-# pass daily prices for optmisation
-for day_idx in range(0, len(price_data), 168):
-    print(day_idx)
-
-    # call optmise method - storing pyomo model
-    mod = a.optimise(
-        price_data[day_idx : day_idx + 168],
-        soc,
-        current_cycle,
-        remaining_capacity,
-        previous_ep_power,
-    )
-
-    model_results = {}
-
-    # loop through the vars
-    for idx, v in enumerate(mod.component_objects(Var, active=True)):
-        # print(idx, v.getname())
-
-        var_val = getattr(mod, str(v))
-
-        model_results[f"{v.getname()}"] = var_val[:].value
-
-    # store data in dataframe
-    if day_idx == 0:
-        df = pd.DataFrame(model_results)
-    else:
-        df = pd.concat([df, pd.DataFrame(model_results)])
-
-    # store soc for next 'episode'
-    soc = df["soc"].iloc[-1]
-
-    # store cycle to carry forward for cumlative calculation
-    current_cycle = df["cumlative_cycle_rate"].iloc[-1]
-
-    a.previous_cap = remaining_capacity
-
-    # update alpha degradation after 'episode'
-    remaining_capacity = a.update_remaining_cap(current_cycle)
-
-    previous_ep_power = abs(
-        np.sum(df["energy_in"].iloc[-168:] + df["energy_out"].iloc[-168:])
-    )
-
-
-# update cumlative profit so ensure profits carried between episodes
-df["cumlative_profit"] = df["profit_timeseries"].cumsum()
-
-# save profits for runtime duration (for comparison with DQN models)
-df.to_csv("results/timeseries_results_MILP.csv")
-
-plt.plot(df["cumlative_profit"].values)
-plt.show()
-
-df.to_clipboard()
